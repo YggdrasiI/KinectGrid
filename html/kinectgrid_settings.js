95b60eb7b2ec6ed814713335a5a1ae836e9a0879
Olaf Schulz <yggtrash@googlemail.com>
2012-07-05 15:30:16 +0200

    Readd renamed files...

---
+++ html/kinectgrid_settings.js
@@ -0,0 +1,12 @@
+var json_kinect = {{ONION_JSON_KINECT}};    
+
+$(function(){
+		//Add hidden input field stub and create form
+		a = deepCopy(json_kinect);
+
+		//b ={"name":"json","id":"json","type":"text","size":"100","value":JSON.stringify(json_kinect)};
+		//a.html[a.html.length] = b;
+
+    $("#settingKinectForm").dform(a);
+});
+
